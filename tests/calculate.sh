708ade624d59ea8d2eaf533945f89ab51ee3b8b0
Beatriz Lujan <bibilujan@gmail.com>
2021-12-22 16:57:22 -0500

    add file check to the regression test

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -8,3 +8,6 @@ cd $1
 
 #get md5sum for output file ignoring two lines
 cat qc_metrics.json | grep -v "maxEstCor\"\|maxTruCor\"" | md5sum
+
+#check that all files were produced
+ls | perl -pe  's/.*?\.//' | sort | uniq -c
